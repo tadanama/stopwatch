3341659c44b3a36b22b10d48248b6521253c0a3c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-01 14:44:54 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Stopwatch from "./components/Stopwatch.jsx";
+
+function App() {
+	return (
+		<>
+			<Stopwatch />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Stopwatch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Stopwatch() {
+	return <h1>Stopwatch</h1>;
+}
+
+export default Stopwatch;
