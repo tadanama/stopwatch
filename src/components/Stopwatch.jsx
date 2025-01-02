b6c8d42bd659df1ecfe79df2d6b2c62f74990f17
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-02 08:54:18 +0800

    Start or stop the setInterval depending on the isRunning state

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -6,6 +6,23 @@ function Stopwatch() {
 	const [time, setTime] = useState(0); // Track the time
 	const [isRunning, setIsRunning] = useState(false); // Determine to start the interval or clear
 
+	// Start interval when isRunning is true
+	// Stop interval when isRunning is false
+	useEffect(() => {
+		let interval;
+
+		if (isRunning) {
+			// Set an interval that runs a callback function every 10ms
+			interval = setInterval(() => {
+				// Increment time every 10ms
+				setTime((prev) => prev + 10);
+			}, 10);
+		} else {
+			// Stop the interval when is Running is false
+			clearInterval(interval);
+		}
+	}, [isRunning]);
+
 	return (
 		<>
 			<div className="stopwatch-container">
